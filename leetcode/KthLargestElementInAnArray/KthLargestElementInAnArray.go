64ee75abd4ec2e7837f4da6c2f0ebf3168b31382
hz <936432896@qq.com>
2018-10-28 21:41:16 +0800

    todo: quick sort

---
+++ leetcode/KthLargestElementInAnArray/KthLargestElementInAnArray.go
@@ -0,0 +1,29 @@
+package KthLargestElementInAnArray
+
+/*
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+*/
+
+
+// we can solve this based on quick sort
+func findKthLargest(nums []int, k int) int {
+
+}
+
+func QuickSort(nums []int) {
+
+}
+
+
