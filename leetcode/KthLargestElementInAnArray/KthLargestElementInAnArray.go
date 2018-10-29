1bbdf841a068c860ca8e0e4af2e6f16b02fa06f2
hz <936432896@qq.com>
2018-10-29 20:17:19 +0800

    leetcode: Kth Largest Element In An Array

--- leetcode/KthLargestElementInAnArray/KthLargestElementInAnArray.go
+++ leetcode/KthLargestElementInAnArray/KthLargestElementInAnArray.go
@@ -19,11 +19,46 @@ You may assume k is always valid, 1 ≤ k ≤ array's length.
 
 // we can solve this based on quick sort
 func findKthLargest(nums []int, k int) int {
-
+	return findKth(nums, 0, len(nums), k)
 }
 
-func QuickSort(nums []int) {
+// [left, right)
+func findKth(nums []int, left, right, k int) int {
+	pivot := nums[left]
+	i := left
+	j := right
 
+	for {
+		for {
+			// should add here. or it will be in endless loop if all element is same
+			i++
+			if i < right && nums[i] > pivot {} else {break}
+		}
+		for {
+			j--
+			if j > left && nums[j] < pivot {} else {break}
+		}
+
+		if i < j {
+			//swap
+			tmp := nums[i]
+			nums[i] = nums[j]
+			nums[j] = tmp
+		} else {
+			break
+		}
+	}
+
+	// swap pivot, and divide from pivot
+	nums[left] = nums[j]
+	nums[j] = pivot
+
+	if j == k - 1 {
+		return pivot
+	} else if j < k - 1 {
+		return findKth(nums, j + 1, right, k)
+	} else if j > k - 1 {
+		return findKth(nums, left, j, k)
+	}
+	return -1
 }
-
-
--- leetcode/KthLargestElementInAnArray/KthLargestElementInAnArray_test.go
+++ leetcode/KthLargestElementInAnArray/KthLargestElementInAnArray_test.go
@@ -1,1 +1,11 @@
 package KthLargestElementInAnArray
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestQuickSortSort(t *testing.T) {
+	nums := []int{3,2,1,5,6,4}
+	fmt.Println(findKthLargest(nums, 6))
+}
