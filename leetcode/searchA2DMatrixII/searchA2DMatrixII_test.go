37bbc00e74c430876c0c466e91c00f704e2c5d89
hz <936432896@qq.com>
2018-10-30 20:11:50 +0800

    leetcode: Search a 2D Matrix II

---
+++ leetcode/searchA2DMatrixII/searchA2DMatrixII_test.go
@@ -0,0 +1,13 @@
+package searchA2DMatrixII
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestSearchMatrix(t *testing.T) {
+	//matrix := [][]int{{1,   4,  7, 11, 15}, {2,   5,  8, 12, 19}, {3,   6,  9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}
+
+	matrix := [][]int{{}}
+	fmt.Println(searchMatrix(matrix, 20))
+}
