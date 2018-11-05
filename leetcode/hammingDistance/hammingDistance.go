308e7cb6a3c81bfce3f6a2d87a0b0a71f6593694
hz <936432896@qq.com>
2018-11-05 20:26:15 +0800

    leetcode: Hamming Distance

---
+++ leetcode/hammingDistance/hammingDistance.go
@@ -0,0 +1,34 @@
+package hammingDistance
+
+/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+*/
+
+func hammingDistance(x int, y int) int {
+	xor := x ^ y
+	result := 0
+
+	for 0 != xor {
+		result += xor & 1
+		xor >>= 1
+	}
+	return result
+}
---
+++ leetcode/hammingDistance/hammingDistance_test.go
@@ -0,0 +1,10 @@
+package hammingDistance
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestHammingDistance(t *testing.T) {
+	fmt.Println(hammingDistance(1, 4))
+}
