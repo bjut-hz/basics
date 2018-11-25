360e7e7a68215d00f054b8757189bb64bb81ff5b
hz <936432896@qq.com>
2018-11-25 14:47:31 +0800

    leetcode: Next Permutation

---
+++ leetcode/nextPermutation/nextPermutation.go
@@ -0,0 +1,40 @@
+package nextPermutation
+
+/*
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+The replacement must be in-place and use only constant extra memory.
+
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+*/
+
+
+func nextPermutation(nums []int)  {
+	if len(nums) <= 1 { return }
+
+	i := len(nums) - 2
+	j := len(nums) - 1
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+
+	if i >= 0 {
+		for nums[j] <= nums[i] { j-- }
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+
+	// reverse
+	m := i + 1
+	n := len(nums) - 1
+	for m < n {
+		nums[m], nums[n] = nums[n], nums[m]
+		m++
+		n--
+	}
+}
