9c130d0b567eb8702285ff4a74ad21964778251d
hz <936432896@qq.com>
2018-11-30 17:22:57 +0800

    leetcode: Plus One

---
+++ leetcode/plusOne/plusOne.go
@@ -0,0 +1,39 @@
+package plusOne
+
+/*
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+*/
+
+func plusOne(digits []int) []int {
+	if 0 == len(digits) { return []int{} }
+
+	result := make([]int, 0, len(digits) + 1)
+	carry := 1
+	for i := len(digits) - 1; i >= 0; i-- {
+		sum := digits[i] + carry
+		carry = sum / 10
+		digits[i] = sum % 10
+	}
+
+	if carry != 0 {
+		result = append(result, carry)
+	}
+	result = append(result, digits...)
+
+	return result
+}
---
+++ leetcode/plusOne/plusOne_test.go
@@ -0,0 +1,10 @@
+package plusOne
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestPlusOne(t *testing.T) {
+	fmt.Println(plusOne([]int{9,9,9}))
+}
