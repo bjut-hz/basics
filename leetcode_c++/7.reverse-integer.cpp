fe9c0e39a092fba140b252cb5ec10d344d71169a
hz <936432896@qq.com>
2019-11-02 16:34:05 +0800

    7.reverse-integer

---
+++ leetcode_c++/7.reverse-integer.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=7 lang=cpp
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        while (x != 0) {
+            res = 10 * res + x % 10;
+            x /= 10;
+        }
+        return (res > INT_MAX || res < INT_MIN) ? 0 : res;
+    }
+};
+// @lc code=end
