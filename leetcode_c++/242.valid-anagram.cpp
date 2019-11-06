e82e8ad9ac01cfeef4795db68f710352cfb26d10
hz <936432896@qq.com>
2019-11-06 19:09:28 +0800

    242.valid-anagram

---
+++ leetcode_c++/242.valid-anagram.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=242 lang=cpp
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
+    }
+};
+// @lc code=end
+
