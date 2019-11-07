bdf9d260c8176a3976db7a239e51158f0aa51370
hz <936432896@qq.com>
2019-11-07 17:43:29 +0800

    95.unique-binary-search-trees-ii

---
+++ leetcode_c++/95.unique-binary-search-trees-ii.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=95 lang=cpp
+ *
+ * [95] Unique Binary Search Trees II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int start, int end) {
+        if(start == end) {
+            return {new TreeNode(start)};
+        }
+        if(start > end) {
+            return {NULL};
+        }
+        vector<TreeNode*> ans;
+        for(int i = start; i <= end; ++i) {
+            auto left = generateTrees(start, i - 1);
+            auto right = generateTrees(i + 1, end);
+
+            for(auto& l : left) {
+                for(auto& r : right) {
+                    TreeNode* node = new TreeNode(i);
+                    node->left = l;
+                    node->right = r;
+                    ans.push_back(node);
+                }
+            }
+        }
+
+        return ans;
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        if(0 == n) return {};
+        return generateTrees(1, n);
+    }
+};
+// @lc code=end
+
