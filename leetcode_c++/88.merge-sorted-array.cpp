35ea16bab1d531c90369b864e8ded274fdfbf824
hz <936432896@qq.com>
2019-11-14 21:10:28 +0800

    88.merge-sorted-array

---
+++ leetcode_c++/88.merge-sorted-array.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=88 lang=cpp
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+
+        int idx = m + n - 1;
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[idx--] = nums1[i--];
+            } else {
+                nums1[idx--] = nums2[j--];
+            }
+        }
+
+        while(i >= 0) {
+            nums1[idx--] = nums1[i--];
+        }
+
+        while(j >= 0) {
+            nums1[idx--] = nums2[j--];
+        }
+    }
+};
+// @lc code=end
+
