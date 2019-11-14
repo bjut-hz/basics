c487a6fc92d7674a61fb76d7ba4c781a17590aa0
hz <936432896@qq.com>
2019-11-14 14:49:39 +0800

    48.rotate-image

---
+++ leetcode_c++/48.rotate-image.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=48 lang=cpp
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        const int N = matrix.size();
+        for(int i = 0; i < N / 2; ++i) {
+            for(int j = i; j < N - i - 1; ++j) {
+                auto temp = matrix[i][j];
+                matrix[i][j] = matrix[N - j - 1][i];
+                matrix[N - j - 1][i] = matrix[N - i - 1][N - j - 1];
+                matrix[N - i - 1][N - j - 1] = matrix[j][N - i - 1];
+                matrix[j][N - i - 1] = temp;
+            }
+        }
+    }
+};
+// @lc code=end
+
