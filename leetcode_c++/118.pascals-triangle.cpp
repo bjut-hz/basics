4de372c18ebe53cab2d054c369f1895d1803e5c2
hz <936432896@qq.com>
2019-11-15 16:14:11 +0800

    118.pascals-triangle

---
+++ leetcode_c++/118.pascals-triangle.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=118 lang=cpp
+ *
+ * [118] Pascal's Triangle
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows, vector<int>());
+        for(int i = 0; i < numRows; ++i) {
+            res[i].resize(i + 1, 1);
+            for(int j = 1; j < i; ++j) {
+                res[i][j] = res[i - 1][j - 1] + res[i-1][j];
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
