303c69566b3a2621f47462dcf92ca9b9487e302b
hz <936432896@qq.com>
2019-11-27 18:38:00 +0800

    384.shuffle-an-array

---
+++ leetcode_c++/384.shuffle-an-array.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=384 lang=cpp
+ *
+ * [384] Shuffle an Array
+ *
+ * https://leetcode.com/problems/shuffle-an-array/description/
+ *
+ * algorithms
+ * Medium (51.28%)
+ * Likes:    352
+ * Dislikes: 805
+ * Total Accepted:    96.3K
+ * Total Submissions: 187.8K
+ * Testcase Example:  '["Solution","shuffle","reset","shuffle"]\n[[[1,2,3]],[],[],[]]'
+ *
+ * Shuffle a set of numbers without duplicates.
+ * 
+ * 
+ * Example:
+ * 
+ * // Init an array with set 1, 2, and 3.
+ * int[] nums = {1,2,3};
+ * Solution solution = new Solution(nums);
+ * 
+ * // Shuffle the array [1,2,3] and return its result. Any permutation of
+ * [1,2,3] must equally likely to be returned.
+ * solution.shuffle();
+ * 
+ * // Resets the array back to its original configuration [1,2,3].
+ * solution.reset();
+ * 
+ * // Returns the random shuffling of array [1,2,3].
+ * solution.shuffle();
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    // 水塘抽样算法: https://www.cnblogs.com/krcys/p/9121487.html
+    Solution(vector<int>& nums) {
+        nums_ = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return nums_;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int> res = nums_;
+
+        for(int i = 0; i < res.size(); ++i) {
+            int idx = i + rand() % (res.size() - i);
+            swap(res[i], res[idx]);
+        }
+        return res;
+    }
+
+private:
+    vector<int> nums_;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
+// @lc code=end
+
