daed1fe6f9e97f3041e8143f10ddf7695c0babd9
hz <936432896@qq.com>
2019-11-29 15:34:57 +0800

    337.house-robber-iii

---
+++ leetcode_c++/337.house-robber-iii.cpp
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode id=337 lang=cpp
+ *
+ * [337] House Robber III
+ *
+ * https://leetcode.com/problems/house-robber-iii/description/
+ *
+ * algorithms
+ * Medium (49.26%)
+ * Likes:    1896
+ * Dislikes: 38
+ * Total Accepted:    124.2K
+ * Total Submissions: 252.2K
+ * Testcase Example:  '[3,2,3,null,3,null,1]'
+ *
+ * The thief has found himself a new place for his thievery again. There is
+ * only one entrance to this area, called the "root." Besides the root, each
+ * house has one and only one parent house. After a tour, the smart thief
+ * realized that "all houses in this place forms a binary tree". It will
+ * automatically contact the police if two directly-linked houses were broken
+ * into on the same night.
+ * 
+ * Determine the maximum amount of money the thief can rob tonight without
+ * alerting the police.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,2,3,null,3,null,1]
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \   \ 
+ * ⁠    3   1
+ * 
+ * Output: 7 
+ * Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,4,5,1,3,null,1]
+ * 
+ * 3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \   \ 
+ * ⁠1   3   1
+ * 
+ * Output: 9
+ * Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        map<TreeNode*, int> m;
+        
+        return rob(root, m);
+    }
+
+    int rob(TreeNode* root, map<TreeNode*, int>& mem) {
+        if(nullptr == root) return 0;
+
+        if(mem.count(root)) return mem[root];        
+        int val = 0;
+        if(root->left) {
+            val += rob(root->left->left, mem);
+            val += rob(root->left->right, mem);
+        }
+        if(root->right) {
+            val += rob(root->right->left, mem);
+            val += rob(root->right->right, mem);
+        }
+
+        int res = max(root->val + val, rob(root->left, mem) + rob(root->right, mem));
+        mem[root] = res;
+        return res;
+    }
+};
+// @lc code=end
+
