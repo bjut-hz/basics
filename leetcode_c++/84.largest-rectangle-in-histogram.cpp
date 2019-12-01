056f95782538251b5c54a084cffe1850f2d30858
hz <936432896@qq.com>
2019-12-01 17:01:35 +0800

    84.largest-rectangle-in-histogram

--- leetcode_c++/84.largest-rectangle-in-histogram.cpp
+++ leetcode_c++/84.largest-rectangle-in-histogram.cpp
@@ -7,6 +7,8 @@
 // @lc code=start
 class Solution {
 public:
+    // 使用单调递增栈,注意处理到栈里的某个元素的时候,还在栈里的元素与该元素之间的值都是大于该元素的而且，其后的到待处理的i位置的元素也是大于该元素的
+    // 比如实例中在处理元素1的时候，那么此前处理的栈中的元素都是大于1,并且其后已经处理的元素也是大于该值的
     int largestRectangleArea(vector<int>& heights) {
         heights.push_back(0); // 方便处理最后一个元素
 
