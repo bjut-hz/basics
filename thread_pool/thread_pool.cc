f731273d78ac380bfd0a9c32892fad2f667adcdf
hz <936432896@qq.com>
2019-12-11 16:09:43 +0800

    format

--- thread_pool/thread_pool.cc
+++ thread_pool/thread_pool.cc
@@ -4,31 +4,31 @@
 #include <assert.h>
 
 int Add(int a, int b, int& sum) {
-	sum = a + b;
-	return a + b;
+    sum = a + b;
+    return a + b;
 }
 
 void SayHello() {
-	std::cout << "hello world" << std::endl;
+    std::cout << "hello world" << std::endl;
 }
-// 最好用io密集的任务来测试
+// 鏈�濂界敤io瀵嗛泦鐨勪换鍔℃潵娴嬭瘯
 int main() {
-	//std::cout << "add number with one single thread ..." << std::endl;
-	//auto t1 = std::chrono::system_clock::now();
+    //std::cout << "add number with one single thread ..." << std::endl;
+    //auto t1 = std::chrono::system_clock::now();
 
-	//auto t2 = std::chrono::system_clock::now();
-	//std::cout << (t2 - t1).count() << " tick count..  " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " microseconds" << std::endl;
+    //auto t2 = std::chrono::system_clock::now();
+    //std::cout << (t2 - t1).count() << " tick count..  " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " microseconds" << std::endl;
 
 
-	//std::cout << "add number with thread pool ..." << std::endl;
-	//auto t3 = std::chrono::system_clock::now();
+    //std::cout << "add number with thread pool ..." << std::endl;
+    //auto t3 = std::chrono::system_clock::now();
 
-	//auto t4 = std::chrono::system_clock::now();
-	//std::cout << (t4 - t3).count() << " tick count..  " << std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count() << " microseconds" << std::endl;
+    //auto t4 = std::chrono::system_clock::now();
+    //std::cout << (t4 - t3).count() << " tick count..  " << std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count() << " microseconds" << std::endl;
 
-	// with return value test
-	ThreadPool pool(2);
-	int sum = 0;
-	auto future = pool.Sumbit(Add, 1, 2, std::ref(sum));
-	assert(future.get() == sum);
+    // with return value test
+    ThreadPool pool(2);
+    int sum = 0;
+    auto future = pool.Sumbit(Add, 1, 2, std::ref(sum));
+    assert(future.get() == sum);
 }
--- thread_pool/thread_pool.h
+++ thread_pool/thread_pool.h
@@ -12,80 +12,80 @@
 
 class ThreadPool {
 public:
-	explicit ThreadPool(int size) {
-		data_ = std::make_shared<data>();
+    explicit ThreadPool(int size) {
+        data_ = std::make_shared<data>();
 
-		for(int i = 0; i < size; ++i) {
-			std::thread(
-				// 这里不能用引用,因为需要保证线程池被析构后,data对象依然不被析构
-				[data = data_](){
-					std::unique_lock<std::mutex> lock(data->mu_);
-					for(;;) {
-						if(!data->tasks_.empty()) {
-							auto task = std::move(data->tasks_.front());
-							data->tasks_.pop();
+        for(int i = 0; i < size; ++i) {
+            std::thread(
+                // 这里不能用引用,因为需要保证线程池被析构后,data对象依然不被析构
+                [data = data_](){
+                    std::unique_lock<std::mutex> lock(data->mu_);
+                    for(;;) {
+                        if(!data->tasks_.empty()) {
+                            auto task = std::move(data->tasks_.front());
+                            data->tasks_.pop();
 
-							// 空出锁供使用
-							lock.unlock();
-							task();
-							lock.lock();
-						} else if(data->shut_down) {
-							break;
-						} else {
-							data->cv.wait(lock);
-						}
-					}
-				}
-			).detach();
-		}
-	}
+                            // 空出锁供使用
+                            lock.unlock();
+                            task();
+                            lock.lock();
+                        } else if(data->shut_down) {
+                            break;
+                        } else {
+                            data->cv.wait(lock);
+                        }
+                    }
+                }
+            ).detach();
+        }
+    }
 
-	~ThreadPool() {
-		if(data_) {
-			{
-				std::lock_guard<std::mutex> lock(data_->mu_);
-				data_->shut_down = true;
-			}
-		}
+    ~ThreadPool() {
+        if(data_) {
+            {
+                std::lock_guard<std::mutex> lock(data_->mu_);
+                data_->shut_down = true;
+            }
+        }
 
-		data_->cv.notify_all();
-	}
+        data_->cv.notify_all();
+    }
 
-	// void(void) template specialization
-	template <typename Func>
-	void Sumbit(Func&& task) {
-		{
-			std::lock_guard<std::mutex> lock(data_->mu_);
-			data_->tasks_.emplace(std::forward<Func>(task));
-		}
-		data_->cv.notify_one();
-	}
+    // void(void) template specialization
+    template <typename Func>
+    void Sumbit(Func&& task) {
+        {
+            std::lock_guard<std::mutex> lock(data_->mu_);
+            data_->tasks_.emplace(std::forward<Func>(task));
+        }
+        data_->cv.notify_one();
+    }
 
-	template <typename R, typename... Args>
-	auto Sumbit(R&& f, Args&& ...args) -> std::future<decltype(f(args...))> {
-		auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>( 
-			std::bind(std::forward<R>(f), std::forward<Args>(args)...)
-		);
-		{
-			std::lock_guard<std::mutex> lock(data_->mu_);
-			// 包装成void(void)类型
-			data_->tasks_.emplace([task_ptr](){
-				(*task_ptr)();
-			});
-		}
-		data_->cv.notify_one();
-		return task_ptr->get_future();
-	}
+    template <typename R, typename... Args>
+    auto Sumbit(R&& f, Args&& ...args) -> std::future<decltype(f(args...))> {
+        auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>( 
+            std::bind(std::forward<R>(f), std::forward<Args>(args)...)
+        );
+        {
+            std::lock_guard<std::mutex> lock(data_->mu_);
+            // 包装成void(void)类型
+            data_->tasks_.emplace([task_ptr](){
+                (*task_ptr)();
+            });
+        }
+        data_->cv.notify_one();
+        return task_ptr->get_future();
+    }
 
 private:
-	struct data {
-		std::condition_variable 				cv;
-		std::mutex								mu_;
-		std::queue<std::function<void(void)>> 		tasks_;
-		bool									shut_down = false;
-	};
+    struct data {
+        std::condition_variable 					cv;
+        std::mutex									mu_;
+        std::queue<std::function<void(void)>> 		tasks_;
+        bool										shut_down = false;
+    };
 
-	std::shared_ptr<data> data_;
+    std::shared_ptr<data> data_;
 };
 
 #endif // THREAD_POOL_H
